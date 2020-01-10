e93ccf9b59808181f398b2eddd2eb1035098bc44
CharlieMansion <mansion.ch@gmail.com>
2020-01-11 00:54:20 +0100

    Updates

---
+++ precache-manifest.e789e6c353bc9473f74c2534714610cf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c58fc52cb0d611a3bb9e3acef12afe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0355e4ddaba7df2ea1b4",
+    "url": "/static/css/2.1af7a3c5.chunk.css"
+  },
+  {
+    "revision": "26ee058c0f7ae18f6976",
+    "url": "/static/css/main.d082f22c.chunk.css"
+  },
+  {
+    "revision": "0355e4ddaba7df2ea1b4",
+    "url": "/static/js/2.10cfd684.chunk.js"
+  },
+  {
+    "revision": "b16c193132695216b3273f2acb3f96c7",
+    "url": "/static/js/2.10cfd684.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26ee058c0f7ae18f6976",
+    "url": "/static/js/main.cabaaa34.chunk.js"
+  },
+  {
+    "revision": "315867e89a3f904b094a",
+    "url": "/static/js/runtime-main.665fc9da.js"
+  },
+  {
+    "revision": "1e8497969efb80e53f773c106b154cf4",
+    "url": "/static/media/NN_LOGO.1e849796.png"
+  },
+  {
+    "revision": "ea66bf8234bbe1ee203ced361c102a48",
+    "url": "/static/media/PIC3.ea66bf82.jpg"
+  },
+  {
+    "revision": "4d0639a7695d00170c28fa336505a8a0",
+    "url": "/static/media/dribbble-icon-dark-gray.4d0639a7.png"
+  },
+  {
+    "revision": "e26d94c97c4939decdcfffeded34022f",
+    "url": "/static/media/github-icon-dark-gray.e26d94c9.png"
+  },
+  {
+    "revision": "2a632834ce51b282c70da4c47a9e5423",
+    "url": "/static/media/linkedin-icon-dark-gray.2a632834.png"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/static/media/menu-bar.86595150.svg"
+  },
+  {
+    "revision": "f46146e4ddd17cc92a3c36e55394ea4a",
+    "url": "/static/media/portfolio.f46146e4.mp4"
+  }
+]);
