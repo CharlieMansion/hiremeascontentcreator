57fb9862b1c10441ee5af14b150864075b0736f1
CharlieMansion <mansion.ch@gmail.com>
2020-01-11 16:54:49 +0100

    adding new pic

--- src/comps/avatar/avatar.comp.js
+++ src/comps/avatar/avatar.comp.js
@@ -1,22 +1,23 @@
 import React from 'react';
 //import avatar2 from './PIC1.png';
-//import avatar from './PIC2.png';
-import avatar3 from './PIC3.jpg';
+//import avatar4 from './PIC2.png';
+//import avatar3 from './PIC3.jpg';
+import avatar from './PIC4.jpg';
 import './avatar.styles.scss';
 
 class Avatar extends React.Component {
     constructor() {
         super()
         this.state = {
-            avatar: avatar3
+            avatar: avatar
         }
     }
         toggleAvatar = () => {
-            this.setState({avatar:avatar3})
+            this.setState({avatar:avatar})
         }
 
         toggleAvatarAgain = () => {
-            this.setState({avatar:avatar3})
+            this.setState({avatar:avatar})
         }
 
         render() {
